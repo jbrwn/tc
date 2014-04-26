9ed11cc01426a064012128669f6359509c982245
Joel Brown <joelsbrown1@gmail.com>
2014-04-25 17:51:44 -0700

    TMS service resources

--- TileCook.Web/Controllers/TMSController.cs
+++ TileCook.Web/Controllers/TMSController.cs
@@ -54,11 +54,13 @@ namespace TileCook.Web.Controllers
         [ActionName("Root")]
         public HttpResponseMessage Root()
         {
-            string title = "Tile Map Service";
-            string version = "1.0.0";
-            string href = Request.RequestUri.ToString() + version;
             Services services = new Services();
-            services.Add(new TileMapServiceMetadata(title, version, href));
+            TileMapServiceMetadata metadata = new TileMapServiceMetadata();
+            metadata.title = "Tile Map Service";
+            metadata.version = "1.0.0";
+            metadata.href = Url.Link("TMSService", new { Version = "1.0.0" });
+            services.Add(metadata);
+
             return Request.CreateResponse(HttpStatusCode.OK, services, Configuration.Formatters.XmlFormatter);
         }
 
@@ -66,14 +68,114 @@ namespace TileCook.Web.Controllers
         [ActionName("Service")]
         public HttpResponseMessage Service(string Version)
         {
-            throw new NotImplementedException();
+
+            // Validate version
+            if (!Version.Equals("1.0.0", StringComparison.OrdinalIgnoreCase))
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            TileMapService tileMapService = new TileMapService();
+            tileMapService.version = "1.0.0";
+            tileMapService.services = Url.Link("TMSRoot", new { });
+            //tileMapService.Title = null;
+            //tileMapService.Abstract = null;
+            //tileMapService.KeywordList = null;
+            //tileMapService.ContactInformation = new ContactInformation();
+            //tileMapService.ContactInformation.ContactPersonPrimary = new ContactPersonPrimary();
+            //tileMapService.ContactInformation.ContactPersonPrimary.ContactPerson = null;
+            //tileMapService.ContactInformation.ContactPersonPrimary.ContactOrganization = null;
+            //tileMapService.ContactInformation.ContactPosition = null;
+            //tileMapService.ContactInformation.ContactAddress = new ContactAddress();
+            //tileMapService.ContactInformation.ContactAddress.AddressType = null;
+            //tileMapService.ContactInformation.ContactAddress.Address = null;
+            //tileMapService.ContactInformation.ContactAddress.City = null;
+            //tileMapService.ContactInformation.ContactAddress.StateOrProvince = null;
+            //tileMapService.ContactInformation.ContactAddress.PostCode = null;
+            //tileMapService.ContactInformation.ContactAddress.Country = null;
+            //tileMapService.ContactInformation.ContactVoiceTelephone = null;
+            //tileMapService.ContactInformation.ContactFacsimileTelephone = null;
+            //tileMapService.ContactInformation.ContactElectronicMailAddress = null;
+            tileMapService.TileMaps = new List<TileMapMetadata>();
+            foreach (KeyValuePair<string, Layer> item in LayerCache.GetLayers())
+            {
+                TileMapMetadata tileMapMetadata = new TileMapMetadata();
+                tileMapMetadata.title = item.Value.Title;
+                tileMapMetadata.srs = item.Value.gridset.srs;
+                if (item.Value.gridset.name.Equals("GoogleMapsCompatible", StringComparison.OrdinalIgnoreCase))
+                {
+                    tileMapMetadata.profile = "global-mercator";
+                }
+                else
+                {
+                    tileMapMetadata.profile = "none";
+                }
+                tileMapMetadata.href = Url.Link("TMSTileMap", new { Version = "1.0.0", TileMap = item.Value.name });
+                tileMapService.TileMaps.Add(tileMapMetadata);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, tileMapService, Configuration.Formatters.XmlFormatter);
         }
 
         [HttpGet]
         [ActionName("TileMap")]
         public HttpResponseMessage TileMap(string Version, string TileMap)
         {
-            throw new NotImplementedException();
+            // Validate version
+            if (!Version.Equals("1.0.0", StringComparison.OrdinalIgnoreCase))
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            // Validate layer
+            Layer layer = LayerCache.GetLayer(TileMap);
+            if (layer == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            TileMap tileMap = new TileMap();
+            tileMap.version = "1.0.0";
+            tileMap.tilemapservice = Url.Link("TMSService", new { Version = "1.0.0" });
+            tileMap.Title = layer.Title;
+            //tileMap.Abstract = null;
+            //tileMap.KeywordList = null;
+            //tileMap.Metadata = new Metadata();
+            //tileMap.Metadata.type =null;
+            //tileMap.Metadata.mimetype =null;
+            //tileMap.Metadata.href = null;
+            //tileMap.Attribution = new Attribution();
+            //tileMap.Attribution.Logo = new Logo();
+            //tileMap.Attribution.Logo.width = null;
+            //tileMap.Attribution.Logo.height = null;
+            //tileMap.Attribution.Logo.href= null;
+            //tileMap.WebMapContext = new WebMapContext();
+            //tileMap.WebMapContext.href = null;
+            tileMap.SRS = layer.gridset.srs;
+            tileMap.BoudningBox = new BoundingBox();
+            tileMap.BoudningBox.minx = layer.bounds.minx.ToString();
+            tileMap.BoudningBox.miny = layer.bounds.miny.ToString();
+            tileMap.BoudningBox.maxx = layer.bounds.maxx.ToString();
+            tileMap.BoudningBox.maxy = layer.bounds.maxy.ToString();
+            tileMap.Origin = new Origin();
+            tileMap.Origin.x = layer.bounds.minx.ToString();
+            tileMap.Origin.y = layer.bounds.miny.ToString();
+            tileMap.TileFormat = new TileFormat();
+            tileMap.TileFormat.width = layer.gridset.tileHeight.ToString();
+            tileMap.TileFormat.height = layer.gridset.tileWidth.ToString();
+            tileMap.TileFormat.mimetype = System.Web.MimeMapping.GetMimeMapping("." + layer.formats[0]);
+            tileMap.TileFormat.extension = layer.formats[0];
+            tileMap.TileSets = new TileSets();
+            for(int i=0;i<layer.gridset.grids.Count;i++)
+            {
+                TileSet tileSet = new TileSet();
+                tileSet.href = Url.Link("TMSTileMap", new { Version = "1.0.0", TileMap = layer.name }) + "/" + i.ToString() ;
+                tileSet.unitsperpixel = layer.gridset.resolution(i).ToString();
+                tileSet.order = i.ToString();
+                tileMap.TileSets.Add(tileSet);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, tileMap, Configuration.Formatters.XmlFormatter);
         }
 
     }
--- TileCook.Web/TMSService.cs
+++ TileCook.Web/TMSService.cs
@@ -35,12 +35,6 @@ namespace TileCook.Web.TMSService
     public class TileMapServiceMetadata
     {
         public TileMapServiceMetadata() { }
-        public TileMapServiceMetadata(string title, string version, string href)
-        {
-            this.title = title;
-            this.version = version;
-            this.href = href;
-        }
 
         [XmlAttribute]
         public string title;
@@ -50,5 +44,224 @@ namespace TileCook.Web.TMSService
         public string href;
     }
 
+    public class TileMapService
+    {
+        public TileMapService()
+        {
+        }
+        
+        [XmlAttribute]
+        public string version;
+        [XmlAttribute]
+        public string services;
 
+        public string Title;
+        public string Abstract;
+        public string KeywordList;
+        public ContactInformation ContactInformation;
+
+        [XmlArrayItem("TileMap")]
+        public List<TileMapMetadata> TileMaps;
+
+    }
+
+    public class ContactInformation
+    {
+        public ContactInformation()
+        {
+        }
+
+        public ContactPersonPrimary ContactPersonPrimary;
+        public string ContactPosition;
+        public ContactAddress ContactAddress;
+        public string ContactVoiceTelephone;
+        public string ContactFacsimileTelephone;
+        public string ContactElectronicMailAddress;
+
+    }
+
+    public class ContactPersonPrimary
+    {
+        public ContactPersonPrimary()
+        {
+        }
+        public string ContactPerson;
+        public string ContactOrganization;
+    }
+
+    public class ContactAddress
+    {
+        public ContactAddress()
+        {
+
+        }
+        
+        public string AddressType;
+        public string Address;
+        public string City;
+        public string StateOrProvince;
+        public string PostCode;
+        public string Country;
+    }
+
+    public class TileMapMetadata
+    {
+        public TileMapMetadata()
+        {
+        }
+        
+        [XmlAttribute]
+        public string title;
+        [XmlAttribute]
+        public string srs;
+        [XmlAttribute]
+        public string profile;
+        [XmlAttribute]
+        public string href;
+    }
+
+    public class TileMap
+    {
+        public TileMap()
+        {
+        }
+
+        [XmlAttribute]
+        public string version;
+        [XmlAttribute]
+        public string tilemapservice;
+
+        public string Title;
+        public string Abstract;
+        public string KeywordList;
+        public Metadata Metadata;
+        public Attribution Attribution;
+        public WebMapContext WebMapContext;
+        public string Face;
+        public string SRS;
+        public BoundingBox BoudningBox;
+        public Origin Origin;
+        public TileFormat TileFormat;
+        public TileSets TileSets;
+    }
+
+    public class WebMapContext
+    {
+        public WebMapContext() { }
+
+        [XmlAttribute]
+        public string href;
+    }
+
+    public class Metadata
+    {
+        public Metadata() { }
+
+        [XmlAttribute]
+        public string type;
+        [XmlAttribute("mime-type")]
+        public string mimetype;
+        [XmlAttribute]
+        public string href;
+
+    }
+
+    public class Attribution
+    {
+        public Attribution() { }
+        
+        public string Title;
+        public Logo Logo;
+    }
+
+    public class Logo
+    {
+        public Logo() { }
+        
+        [XmlAttribute]
+        public string width;
+        [XmlAttribute]
+        public string height;
+        [XmlAttribute]
+        public string href;
+        [XmlAttribute("mime-type")]
+        public string mimetype;
+    }
+
+    public class BoundingBox
+    {
+        public BoundingBox() { }
+        
+        [XmlAttribute]
+        public string minx;
+        [XmlAttribute]
+        public string miny;
+        [XmlAttribute]
+        public string maxx;
+        [XmlAttribute]
+        public string maxy;
+    }
+
+    public class Origin
+    {
+        public Origin() { }
+
+        [XmlAttribute]
+        public string x;
+        [XmlAttribute]
+        public string y;
+    }
+
+    public class TileFormat
+    {
+        public TileFormat() { }
+        
+        [XmlAttribute]
+        public string width;
+        [XmlAttribute]
+        public string height;
+        [XmlAttribute("mime-type")]
+        public string mimetype;
+        [XmlAttribute]
+        public string extension;
+    }
+
+    [XmlRoot("TileSets")]
+    public class TileSets : IEnumerable<TileSet>
+    {
+        List<TileSet> _tilesets;
+        
+        public TileSets()
+        {
+            this._tilesets = new List<TileSet>();
+        }
+
+        [XmlAttribute]
+        public string profile;
+
+        public void Add(TileSet item)
+        {
+            this._tilesets.Add(item);
+        }
+
+        public IEnumerator<TileSet> GetEnumerator()
+        {
+            return this._tilesets.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public class TileSet
+    {
+        [XmlAttribute]
+        public string href;
+        [XmlAttribute("units-per-pixel")]
+        public string unitsperpixel;
+        [XmlAttribute]
+        public string order;
+    }
 }
