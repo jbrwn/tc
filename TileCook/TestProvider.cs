8be9612cbf6ed6b76fb26646ab67618ae49da5df
Joel Brown <joelsbrown1@gmail.com>
2014-05-04 22:12:42 -0700

    update TestProvider

--- TileCook/TestProvider.cs
+++ TileCook/TestProvider.cs
@@ -12,64 +12,37 @@ namespace TileCook
     [DataContract]
     public class TestProvider : IProvider
     {
+        private byte[] imgBytes;
+        
         public byte [] render(Envelope envelope, string format, int tileWidth, int tileHeight)
         {
-
-            byte[] imgBytes;
-            using (Image img = new Bitmap(tileWidth, tileHeight))
+            if (imgBytes == null)
             {
-                using (var font = new Font("Arial", 10))
-                using (var stringFormat = new StringFormat())
-                using (Brush fill = new SolidBrush(Color.LightBlue))
-                using (var g = Graphics.FromImage(img))
+                using (Bitmap img = new Bitmap(tileWidth, tileHeight))
                 {
-                    
+                    using (Brush background = new SolidBrush(Color.White))
+                    using (Pen border = new Pen(new SolidBrush(Color.FromArgb(75, 255, 0, 0)), 50))
+                    using (var g = Graphics.FromImage(img))
+                    {
+                        var rect = new Rectangle(0, 0, tileWidth, tileHeight);
+                        g.SmoothingMode = SmoothingMode.AntiAlias;
+                        g.FillRectangle(background, rect);
+                        g.DrawRectangle(border, rect);
+                    }
 
-                    var rect = new RectangleF(2, 2, tileWidth-4, tileHeight-4);
-                    g.FillRectangle(fill, rect);
-                    
-                    //ul.x
-                    stringFormat.LineAlignment = StringAlignment.Near;
-                    stringFormat.Alignment = StringAlignment.Near;
-                    g.DrawString(envelope.minx.ToString(), new Font("Arial", 10), SystemBrushes.WindowText, rect, stringFormat);
-                   
-                    //lr.y
-                    stringFormat.LineAlignment = StringAlignment.Far;
-                    stringFormat.Alignment = StringAlignment.Far;
-                    g.DrawString(envelope.miny.ToString(), new Font("Arial", 10), SystemBrushes.WindowText,rect, stringFormat);
-
-                    rect = new RectangleF(2, 20, tileWidth - 4, tileHeight - 40);
-                    
-                    //ul.y
-                    stringFormat.LineAlignment = StringAlignment.Near;
-                    stringFormat.Alignment = StringAlignment.Near;
-                    g.DrawString(envelope.maxy.ToString(), new Font("Arial", 10), SystemBrushes.WindowText,rect, stringFormat);
-
-                    //lr.x
-                    stringFormat.LineAlignment = StringAlignment.Far;
-                    stringFormat.Alignment = StringAlignment.Far;
-                    g.DrawString(envelope.maxx.ToString(), new Font("Arial", 10), SystemBrushes.WindowText,rect, stringFormat);
-
-
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        img.Save(ms, ImageFormat.Png);
+                        this.imgBytes = ms.ToArray();
+                    }
                 }
-                
-                
-                using (MemoryStream ms = new MemoryStream())
-                {
-                    img.Save(ms, ImageFormat.Png);
-                    imgBytes = ms.ToArray();
-                }
-
             }
-
-            return imgBytes;
-
+            return this.imgBytes;
         }
 
         public List<string> getFormats()
         {
             return new List<string> {"png"};
         }
-
     }
 }
