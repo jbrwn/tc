c07b89d01b0ac7bc38e08f5f625901138940821d
Joel Brown <joelsbrown1@gmail.com>
2014-05-09 21:38:43 -0700

    update disk cache to intelligently poll for file access

--- TileCook/DiskCache.cs
+++ TileCook/DiskCache.cs
@@ -3,55 +3,114 @@ using System.Collections.Generic;
 using System.Text;
 using System.IO;
 using System.Runtime.Serialization;
+using System.Threading;
 
 namespace TileCook
 {
     [DataContract]
     public class DiskCache : ICache
     {
+        private const int ACCESS_POLL_COUNT = 5;
+        private const int ACCESS_POLL_INTERVAL = 100; //100 milliseconds
 
         private DiskCache() { }
 
         public DiskCache(string directory)
-            : this(directory, 0) { }
-
-        public DiskCache(string directory, int lifetime)
         {
             this.directory = directory;
-            this.lifetime = lifetime;
         }
         
         [DataMember(IsRequired=true)]
         public string directory { get; set; }
 
-        //lifetime
-        [DataMember]
-        public int lifetime { get; set; }
-
         public byte[] get(int z, int x, int y, string format)
         {
             string path = this.buildPath(z, x, y, format);
             byte[] img = null;
-            try
+
+            int i = 0;
+            while (true)
             {
-                img = File.ReadAllBytes(path);
+                try
+                {
+                    img = File.ReadAllBytes(path);
+                    break;
+                }
+                catch (FileNotFoundException e) { break; }
+                catch (DirectoryNotFoundException e) { break; }
+                catch (IOException e)
+                {   
+                    //is file being written? check for ERROR_SHARING_VIOLATION (HRESULT = -2147024864)
+                    //.NET 4.5 required to access HResult directly
+                    if (e.HResult == -2147024864 && i <= ACCESS_POLL_COUNT)
+                    {
+                        i++;
+                        Thread.Sleep(ACCESS_POLL_INTERVAL);
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
             }
-            catch (FileNotFoundException e) { }
-            catch (DirectoryNotFoundException e) { }
             return img;
         }
 
         public void put(int z, int x, int y, string format, byte[] image)
         {
             string path = this.buildPath(z, x, y, format);
-            Directory.CreateDirectory(Path.GetDirectoryName(path));
-            File.WriteAllBytes(path, image);
+            int i = 0;
+            while (true)
+            {
+                try
+                {
+                    Directory.CreateDirectory(Path.GetDirectoryName(path));
+                    File.WriteAllBytes(path, image);
+                    break;
+                }
+                catch (IOException e)
+                {
+                    //is file being written? check for ERROR_SHARING_VIOLATION (HRESULT = -2147024864)
+                    //.NET 4.5 required to access HResult directly
+                    if (e.HResult == -2147024864 && i <= ACCESS_POLL_COUNT)
+                    {
+                        i++;
+                        Thread.Sleep(ACCESS_POLL_INTERVAL);
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
         }
 
         public void delete(int z, int x, int y, string format)
         {
             string path = this.buildPath(z, x, y, format);
-            File.Delete(path);
+            int i = 0;
+            while (true)
+            {
+                try
+                {
+                    File.Delete(path);
+                    break;
+                }
+                catch (IOException e)
+                {
+                    //is file being written? check for ERROR_SHARING_VIOLATION (HRESULT = -2147024864)
+                    //.NET 4.5 required to access HResult directly
+                    if (e.HResult == -2147024864 && i <= ACCESS_POLL_COUNT)
+                    {
+                        i++;
+                        Thread.Sleep(ACCESS_POLL_INTERVAL);
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
         }
 
         private string buildPath(int z, int x, int y, string format)
