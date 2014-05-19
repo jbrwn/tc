17cfdf3a13ad9f962d89605d4e70ae4ab21aa7bd
Joel Brown <joelsbrown1@gmail.com>
2014-05-18 22:16:40 -0700

    add default png and jpeg options to mapnik provider construcutor

--- TileCook/MapnikProvider.cs
+++ TileCook/MapnikProvider.cs
@@ -19,6 +19,8 @@ namespace TileCook
         public MapnikProvider(string xmlConfig)
         {
             this.xmlConfig = xmlConfig;
+            this.pngOptions = "png";
+            this.jpegOptions = "jpeg";
             
             _map = new Map();
             _map.LoadMap(xmlConfig);
