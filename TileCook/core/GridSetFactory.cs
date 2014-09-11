63acb8eee9122c47b4a6a6c9bd00ff196c17b785
Joel Brown <joelsbrown1@gmail.com>
2014-09-11 16:04:01 -0600

    add gridsetfactory

---
+++ TileCook/core/GridSetFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TileCook.core
+{
+    public static class GridSetFactory
+    {
+
+        public static IGridSet CreateGridSet() 
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IGridSet CreateGridSet(string name, string srs, Envelope envelope, int tileSize)
+        {
+            return CreateGridSet(name, srs, envelope, tileSize, 0, false);
+        }
+
+        public static IGridSet CreateGridSet(string name, string srs, Envelope envelope, int tileSize, int pixelSize, bool topOrigin)
+        {
+            throw new NotImplementedException();
+        }
+
+        private IGridSet GoogleMapsCompatible()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
